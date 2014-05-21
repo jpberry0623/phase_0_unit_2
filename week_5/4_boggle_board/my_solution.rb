e81aff34634deac8e4902c0b6a3a85410248cda7
John Berry <jpberry0623@gmail.com>
2014-05-20 20:08:07 -0400

    started working onsolution, not done

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -1,6 +1,6 @@
 # U2.W5: A Nested Array to Model a Boggle Board
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself.
 
 boggle_board = [["b", "r", "a", "e"],
                 ["i", "o", "d", "t"],
@@ -11,14 +11,28 @@ boggle_board = [["b", "r", "a", "e"],
 # Part 1: Access multiple elements of a nested array
 
 # Pseudocode
+# define method that takes two arguments, on for playing board and another for letter position
+# create block to pass array indexs in to identify letters and then join to form a  word
+
+
 
 # Initial Solution
+
+  def create_word(board, *coords)
+    coords.map { |coord| board[coord.first][coord.last]}.join("")
+  end
+
+  puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"  
+  puts create_word(boggle_board, [0,1], [0,2], [1,2])  #=> creates what california slang word?
 
 
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+
+puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"  
+puts create_word(boggle_board, [0,1], [0,2], [1,2])  #=> creates what california slang word?
 
 
 # Reflection 
@@ -28,14 +42,29 @@ boggle_board = [["b", "r", "a", "e"],
 # Part 2: Write a method that takes a row number and returns all the elements in the row.  
 
 # Pseudocode
+#define method that takes row position(integer) as argument
+#playing board(array of arrays) equal to local variable
+# used index of local variable to locate desired row.
+
 
 # Initial Solution
+def get_row(row)
+	boggle_board = [["b", "r", "a", "e"],
+                ["i", "o", "d", "t"],
+                ["e", "c", "l", "r"],
+                ["t", "a", "k", "e"]]
+    boggle_board[row]
+end
+
+
 
 
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+
+p get_row(1) #=>  ["i", "o", "d", "t"]
 
 
 # Reflection 
@@ -47,14 +76,27 @@ boggle_board = [["b", "r", "a", "e"],
 # Part 3: Now write a method that takes a column number and returns all the elements in the column.
 
 # Pseudocode
+# define method that takes a column number(integer) as an argument
+# set boggle board equal to array of playing strings
+# pass method to board to get desired column
 
 # Initial Solution
+def get_col(col)
+    boggle_board = [["b", "r", "a", "e"],
+                ["i", "o", "d", "t"],
+                ["e", "c", "l", "r"],
+                ["t", "a", "k", "e"]]
+boggle_board.transpose[1]
+
+end
+
+p get_col(1)  #=>  ["r", "o", "c", "a"]
 
 
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
-
+p get_col(1)  #=>  ["r", "o", "c", "a"]
 
 # Reflection 
