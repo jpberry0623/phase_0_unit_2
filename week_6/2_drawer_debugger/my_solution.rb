f5889d20f27ecf4a960afc4e61f64d200eb7b3e6
John Berry <jpberry0623@gmail.com>
2014-05-28 13:18:50 -0400

    Updated my_solution.rb

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Original Code
@@ -25,15 +25,16 @@ def close
 @open = false
 end 
 
-def add_item
+def add_item(item)
 @contents << item
 end
 
-def remove_item(item = @contents.pop) #what is `#pop` doing?
+def remove_item(item = @contents.pop) # the "pop is removing the last item of the arrsay ad returning it"
 @contents.delete(item)
 end
 
-def dump  # what should this method return?
+def dump  # what should this method return? It will return the puts statement stating the
+# drawer is empty, but the method does not actually empty the drawer
 puts "Your drawer is empty."
 end
 
@@ -42,11 +43,12 @@ puts "The drawer contains:"
 @contents.each {|silverware| puts "- " + silverware.type }
 end
 
+end
 
 class Silverware
-attr_reader :type
+attr_reader :type, :clean # added clean to class for bonus section
 
-# Are there any more methods needed in this class?
+# Are there any more methods needed in this class? We need to define a method for cleaning_silverware
 
 def initialize(type, clean = true)
 @type = type
@@ -56,6 +58,10 @@ end
 def eat
 puts "eating with the #{type}"
 @clean = false
+end
+
+def clean_silverware # added method for cleaning
+	@clean = true
 end
 
 end
@@ -80,23 +86,60 @@ silverware_drawer.view_contents
 removed_knife = silverware_drawer.remove_item(sharp_knife)
 removed_knife.eat
 removed_knife.clean_silverware 
+puts silverware_drawer.view_contents
+silverware_drawer.dump
+puts silverware_drawer.view_contents #What should this return?  it will return the puts statement that the 
+# drawer is empty
+
+
+
+
+
+#fork = silverware_drawer.remove_item(fork) fork was removed by earlier method, siverware_drawer.remove_item
+# added by me
+fork = Silverware.new("fork")
+silverware_drawer.add_item(fork)
+silverware_drawer.view_contents
+silverware_drawer.remove_item(fork)
 
 silverware_drawer.view_contents
-silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
 
-
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
 fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+puts fork.clean # returns false becasue we have eaten with fork (fork.eat)
+
+fork.clean_silverware #Will test in driver test code to make sure fork is clean after we pass in our method
+
 
 # DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion failed!" unless yield
+end
+ 
+ my_drawer = Drawer.new
 
 
 
+assert {my_drawer.contents.empty? == true}
 
+large_spoon = Silverware.new("large_spoon")
+my_drawer.add_item(large_spoon)
+my_drawer.view_contents
+
+assert {my_drawer.contents.empty? == false}
+assert {large_spoon.clean == true}
+
+
+# fork test for our clean_silverware method
+
+puts fork.clean == true
 
 
 # 5. Reflection 
+
+
+#This challenge was helpful for me. To go through the tests and figure out where the methods were failing is a great
+#way of finding bugs and it helps me understand why TDD is so valuable.  I still struggle with the idea of writing
+#these tests prior to writing code becasue at this stage because it is hard for me to visualize the code I want to 
+#write before diving in a getting down in an editor.  I need to work on just slowing down and thinking about code.
